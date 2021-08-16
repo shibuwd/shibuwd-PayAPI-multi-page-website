2e2f4e72ae50041cbc42e77430fcbb3d4d931801
Shibu Sarkar <shibusarkar619@gmail.com>
2021-08-16 16:30:51 +0600

    re-design nav menu

--- js/app.js
+++ js/app.js
@@ -4,20 +4,22 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  mobile_menu.classList.toggle('active');
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
-  var scroll_position = window.scrollY;
-  if (scroll_position > 250) {} else {
-    header.style.backgroundColor = 'transparent';
-  }
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#fbfcfe';
+} else {
+		header.style.backgroundColor = 'transparent';
+	}
 });
 
 menu_item.forEach((item) => {
-  item.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-  });
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
 });
